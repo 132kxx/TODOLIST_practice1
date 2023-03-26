92fa1a7f0feef582bfc1e6261943e429a4508cc2
kxx <132kxx@gmail.com>
2023-03-26 12:47:29 +0900

    Initial Commit

---
+++ TODOLIST_practice1/TODOLIST_practice1App.swift
@@ -0,0 +1,17 @@
+//
+//  TODOLIST_practice1App.swift
+//  TODOLIST_practice1
+//
+//  Created by kxx: on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct TODOLIST_practice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
